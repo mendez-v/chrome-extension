c43f8ebd16a70a3d9efcee99d839a14405080d03
Vanessa Mendez <147641417+vanmendez@users.noreply.github.com>
2023-11-10 18:15:06 -0500

    Add tabs permissions to manifest.json and minor modifications to stylesheet and HTML file

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ formEl.addEventListener("submit", function(evt) {
 })
 
 tabBtn.addEventListener("click", function () {
-  chrome.tabs.query( { active:true, currentWindow:true }, function(tabs) {
+  chrome.tabs.query( { active: true, currentWindow: true }, function(tabs) {
     myLeads.push(tabs[0].url)
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads)
